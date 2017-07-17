db12c3791451f4615fa4b459ecc1935cec9fdcd1
bbelderbos <bobbelderbos@gmail.com>
2017-07-17 02:29:34 +0200

    redirect / to /articles

--- pyplanet/urls.py
+++ pyplanet/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'', include('articles.urls')),
     url(r'^articles/', include('articles.urls')),
     url(r'^admin/', admin.site.urls),
 ]
